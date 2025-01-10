808a4595c4891acac6483dafbfba08c1a8d581f1
rocklessg <abdulhafiz.suleiman@decagon.dev>
2025-01-10 18:23:28 +0100

    Post test update

--- HaffardFormService.Api/Data/CustomerInfoRepository.cs
+++ HaffardFormService.Api/Data/CustomerInfoRepository.cs
@@ -4,10 +4,15 @@
     {
         public async Task<CustomerInfoResponse> GetCustomerInfoByIndustryAsync(string industryType)
         {
+            if (string.IsNullOrWhiteSpace(industryType))
+            {
+                throw new ArgumentNullException(nameof(industryType));
+            }
+
             // fetch customer record from the data store. (I deally, the record is fetch from the data base)
             var customerRecord = CustomerDataStore.CustInfo.FirstOrDefault(c =>
                                           c.Industry.Equals(industryType, StringComparison.OrdinalIgnoreCase)) ??
-                                          throw new Exception("Customer record not found");
+                                          throw new Exception("Industry record not found");
 
             // Map the customer record to the response object
             // so that the client only gets the necessary information
